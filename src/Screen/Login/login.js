e956e83b0882a2943ab00199e0eedf1a07c6b079
ayman12015 <flippysort99@outlook.com>
2020-09-12 04:21:56 +0200

    navigation + screen 1 underbuilding

--- src/Screen/Login/login.js
+++ src/Screen/Login/login.js
@@ -5,71 +5,71 @@ import { StyleSheet, View, Text, TouchableOpacity, Image, TextInput, Button } fr
 
 
 function Login(props) {
-  
-    const [value, onChangeText] = React.useState('ادخل رقم الجوال');
-  const { navigation } = props
-  return (
-    <View style={styles.container}>
-       <View style={styles.header}>
-       <Image
-        style={styles.stretch}
-        source={require('../../../assets/images/logo.jpg')}
-      />
-    </View>
-    <View style={styles.footer}>
-    <Text style={styles.title}>تسجيل الدخول</Text>
-    <TextInput
-      style={{ height: 40, width: 288, borderColor: 'gray', borderWidth: 1 }}
-      onChangeText={text => onChangeText(text)}
-      value={value}
-     
-    />
-    <TouchableOpacity
-        style={styles.buttonContainer}
-        onPress={() => navigation.navigate('Check')}>
-        <Text style={styles.buttonText}>تخطي</Text>
-      </TouchableOpacity>
-    
-    </View>
-     
-    <Button
-  onPress={() => navigation.navigate('Check')}
-  title="تأكيد"
-  color="#FF6347"
-  accessibilityLabel="Learn more about this purple button"
-/>
-    </View>
-  
 
-  )
+    const [value, onChangeText] = React.useState('ادخل رقم الجوال');
+    const { navigation } = props
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image
+                    style={styles.stretch}
+                    source={require('../../../assets/images/logo.jpg')}
+                />
+            </View>
+            <View style={styles.footer}>
+                <Text style={styles.title}>تسجيل الدخول</Text>
+                <TextInput
+                    style={{ height: 40, width: 288, borderColor: 'gray', borderWidth: 1 }}
+                    onChangeText={text => onChangeText(text)}
+                    value={value}
+
+                />
+                <TouchableOpacity
+                    style={styles.buttonContainer}
+                    onPress={() => navigation.navigate('Check')}>
+                    <Text style={styles.buttonText}>تخطي</Text>
+                </TouchableOpacity>
+
+            </View>
+
+            <Button
+                onPress={() => navigation.navigate('Check')}
+                title="تأكيد"
+                color="#FF6347"
+                accessibilityLabel="Learn more about this purple button"
+            />
+        </View>
+
+
+    )
 }
 
 const styles = StyleSheet.create({
     container: {
-    flex: 1, 
-      backgroundColor: '#009387'
-  },
-  header: {
-      flex: 1,
-      backgroundColor: '#F0F8FF'
-  },
-  footer: {
-      flex: 1,
-    backgroundColor: '#F0F8FF' 
-  },
-  stretch: {
-    width: 344,
-    height: 200,
-    resizeMode: 'stretch',
-  },
-  title: {
-    padding: 8,
-    fontSize: 22,
-    color: '#F07523'
+        flex: 1,
+        backgroundColor: '#009387'
+    },
+    header: {
+        flex: 1,
+        backgroundColor: '#F0F8FF'
+    },
+    footer: {
+        flex: 1,
+        backgroundColor: '#F0F8FF'
+    },
+    stretch: {
+        width: 344,
+        height: 200,
+        resizeMode: 'stretch',
+    },
+    title: {
+        padding: 8,
+        fontSize: 22,
+        color: '#F07523'
 
-  }
+    }
 
-  
+
 })
 
 export default Login
