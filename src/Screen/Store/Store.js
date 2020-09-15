4671c318da5da4e97754759798f63f4203e247e1
waheedmohannad55578@gmail.com <waheedmohannad55578@gmail.com>
2020-09-15 02:29:17 -0700

    add Catogray

--- src/Screen/Store/Store.js
+++ src/Screen/Store/Store.js
@@ -1,12 +1,81 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, Image, StyleSheet, Dimensions, FlatList, ScrollView } from 'react-native'
+import CartHeader from '../../Components/CartHeader/cartHeader'
+import Product from '../../Components/Product/product'
+
 
 export default class Store extends Component {
     render() {
+        const data = [
+            {
+                key: "1",
+                title: "nihs",
+                image: "../../../image/footer.png"
+            },
+            {
+                key: "2",
+                title: "nihs",
+                image: "../image/footer.png"
+            }, {
+                key: "3",
+                title: "nihs",
+                image: "../image/footer.png"
+            }
+            , {
+                key: "4",
+                title: "nihs",
+                image: "../image/footer.png"
+            }
+
+        ]
         return (
-            <View>
-                <Text> textInComponent </Text>
+            <View style={{ flex: 1 }}>
+                <CartHeader name="كنتاكي"></CartHeader>
+                <View style={styles.container}>
+                    <View style={styles.background}>
+                        <Image style={styles.image} source={require("../../Assets/images/kfc.png")} />
+                    </View>
+                    <View style={styles.list}>
+                        {/* {data.map(() => (
+                            <Product
+                            // key={key}
+                            // data={item}
+                            />
+                        ))} */}
+                        <FlatList
+                            data={data}
+                            numColumns={2}
+                            renderItem={({ item, index, separator }) => (
+                                // <Text>sldf;lsd</Text>
+                                <Product
+                                    key={index}
+                                    data={item}
+                                />
+                            )}
+                        />
+                    </View>
+                </View>
             </View>
         )
     }
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    background: {
+        flex: 1,
+        position: "absolute"
+    },
+    list: {
+        flex: 1,
+        top: 150,
+        // backgroundColor: "red"
+    },
+    image: {
+        height: 200,
+        width: Dimensions.get("window").width,
+        resizeMode: "contain"
+    }
+})
+
